6df855be8a7656e4c796eb8aca57389c1260599d
Caio Galazzo <caio.galazzo@gmail.com>
2026-07-18 19:41:55 -0300

    Corrige codificação dos novos sprites

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tamagon-companion-v0.18.58";
+const CACHE_NAME = "tamagon-companion-v0.18.59";
 const APP_SHELL = ["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./assets/explorations/pantano.png","./assets/explorations/montanha.png","./assets/explorations/vulcao.png","./assets/explorations/usina-eletrica.png","./assets/explorations/dojo-afk.png","./assets/explorations/caverna-congelada.png","./assets/furniture/comfortable-bed.png","./assets/furniture/better-feeder.png","./assets/furniture/upgraded-bathtub.png","./assets/furniture/play-corner.png","./assets/furniture/training-bookshelf.png","./assets/furniture/luxury/royal-bed.png","./assets/furniture/luxury/premium-feeder.png","./assets/furniture/luxury/spa-bathtub.png","./assets/furniture/luxury/deluxe-play-corner.png","./assets/furniture/luxury/pro-training-corner.png","./assets/actions/punching-bag.png","./assets/actions/play-ball.png","./assets/actions/oran-berry.png","./assets/actions/bath-foam.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
